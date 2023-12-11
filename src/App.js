7a4c56c42449991d28dd9e6f7160c027aa213470
SalluuuBhai <md.salmanfaris03@gmail.com>
2023-12-11 21:15:45 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes, useNavigate } from "react-router-dom";
+import "./App.css";
+import { Dashboard, Home, Login, MusicPlayer } from "./components";
+import React, { useEffect, useState } from "react";
+import { app } from "./config/firebase.config";
+import { getAuth } from "firebase/auth";
+import { motion, AnimatePresence } from "framer-motion";
+import { validateUser } from "./api";
+import { useStateValue } from "./context/StateProvider";
+import { actionType } from "./context/reducer";
 
 function App() {
+  const firebaseAuth = getAuth(app);
+  const navigate = useNavigate();
+
+  const [{ user, allSongs, song, isSongPlaying, miniPlayer }, dispatch] =
+    useStateValue();
+
+  const [auth, setAuth] = useState(
+    false || window.localStorage.getItem("auth") === "true"
+  );
+  useEffect(() => {
+    firebaseAuth.onAuthStateChanged((userCred) => {
+      if (userCred) {
+        userCred.getIdToken().then((token) => {
+          // console.log(token);
+          validateUser(token).then((data) => {
+            dispatch({
+              type: actionType.SET_USER,
+              user: data,
+            });
+          });
+        });
+      } else {
+        setAuth(false);
+        window.localStorage.setItem("auth", "false");
+        dispatch({
+          type: actionType.SET_USER,
+          user: null,
+        });
+        navigate("/login");
+      }
+    });
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AnimatePresence mode="wait">
+      <div className="h-auto min-w-[680px] bg-primary flex justify-center items-center">
+        <Routes>
+          <Route path="/login" element={<Login setAuth={setAuth} />} />
+          <Route path="/*" element={<Home />} />
+          <Route path="/dashboard/*" element={<Dashboard />} />
+        </Routes>
+        {isSongPlaying && (
+          <motion.div
+            initial={{ opacity: 0, y: 50 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: 50 }}
+            className={`fixed min-w-[700px] h-26  inset-x-0 bottom-0  bg-cardOverlay drop-shadow-2xl backdrop-blur-md flex items-center justify-center`}
+          >
+            <MusicPlayer />
+          </motion.div>
+        )}
+      </div>
+    </AnimatePresence>
   );
 }
 
